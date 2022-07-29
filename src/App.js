a94965983d1007ca5924689d2cb4e379421246b8
janghyeonc <jhjo@claion.co.kr>
2022-07-29 17:22:07 +0900

    zzzzzzzz

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         zzffzz 제발 자동 빌드 좀 돼라ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
+         zzffzz 제발 자동zzzzzs:ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ
         </p>
         <a
           className="App-link"
