c401ef871a52aa508fa2d6394df9f74697b5f231
janghyeonc <jhjo@claion.co.kr>
2022-07-29 14:52:52 +0900

    config test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          zzzzzzz너무 졸리다
+          zzzzconfigzzz너무 졸리다
         </a>
       </header>
     </div>
